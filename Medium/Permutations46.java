eb12e6edabfa506253d2da75051e1ec72f09443f
srilekhaagasanur <70511903+srilekhaagasanur@users.noreply.github.com>
2024-06-06 20:56:31 +0530

    Update Permutations46.java

--- Medium/Permutations46.java
+++ Medium/Permutations46.java
@@ -22,3 +22,33 @@ class Solution {
         }
     }
 }
+
+
+// 2nd Approach
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        permute(0,nums,ans);
+        return ans;
+    }
+    public void permute(int ind, int nums[], List<List<Integer>> ans){
+        if(ind == nums.length){
+            List<Integer> ds = new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=ind;i<nums.length;i++){
+            swap(i,ind,nums);
+            permute(ind+1,nums,ans);
+            swap(i,ind,nums);
+        }
+    }
+    public void swap(int i,int j,int nums[]){
+        int t = nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+}
