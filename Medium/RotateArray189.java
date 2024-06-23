0b021f20df1a105444be98b2748ff6794cc271ea
srilekhaagasanur <70511903+srilekhaagasanur@users.noreply.github.com>
2024-06-23 19:22:01 +0530

    Create RotateArray189.java

---
+++ Medium/RotateArray189.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k=k%n;
+        if(k==0){
+            return;
+        }
+        int temp[] = new int[k];
+        for(int i=0;i<k;i++){
+            temp[i]=nums[n-(k-i)];
+        }
+        for(int i=n-1;i>=k;i--){
+            nums[i]=nums[i-k];
+        }
+        for(int i=0;i<k;i++){
+            nums[i]=temp[i];
+        }
+    }
+}
